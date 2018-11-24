6490f4cca0d240246f8cac1e924f9f12500126d8
Wojciech Pazda <wojciech.pazda@capgemini.com>
2018-11-24 16:55:00 +0100

    added appointment entity

---
+++ src/main/java/com/hackyeah/mainapp/entities/Appointment.java
@@ -0,0 +1,45 @@
+package com.hackyeah.mainapp.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.jvnet.hk2.config.Element;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@Table(name = "APPOINTMENT")
+@javax.persistence.Entity
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "ID")
+    private String id;
+
+    @Column(name = "SPECIALIZATION")
+    @Enumerated(EnumType.STRING)
+    private Specialization specialization;
+
+    @Column(name = "DAYS_TO_APPOINTMENT")
+    private Integer daysToAppointment;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "SURNAME")
+    private String surname;
+
+    @Column(name = "PHONE")
+    private String phone;
+
+    @Column(name = "CITY")
+    private String city;
+
+    @Column(name = "HOME_NUMBER")
+    private Date homeNumber;
+
+    @Column(name = "POSTAL_CODE")
+    private String postalCode;
+}
---
+++ src/main/java/com/hackyeah/mainapp/entities/Specialization.java
@@ -0,0 +1,8 @@
+package com.hackyeah.mainapp.entities;
+
+public enum Specialization {
+
+    KARDIOLOG,
+    PULMUNOLOG,
+    ENDOKRYNOLOG
+}
